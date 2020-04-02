8e6cbb6b43a1da33d41d7bdb1eeedf8780c12187
Vorobeva35 <60929381+Vorobeva35@users.noreply.github.com>
2020-04-02 23:02:09 +0300

    Add files via upload

---
+++ Vector2d.h
@@ -0,0 +1,42 @@
+
+#pragma once
+#include <string>
+#include <iostream>
+#include <math.h>
+using namespace std;
+class Vector2d
+{
+private:
+	double x, y; //координаты вектора
+public:
+	Vector2d() { x = 0; y = 0; } //создает нулевой вектор
+	Vector2d(double, double); //создание вектора по координатам
+	Vector2d(double, double, double, double); //создание вектора по 2 точкам
+	Vector2d(const Vector2d&); //копирование существующего вектора
+	~Vector2d(); //деструктор
+
+	void setx(double value) { x = value; } //задание координаты х
+	double getx() { return x; }			   //получение значения х
+	void sety(double value) { y = value; } //задание координаты у
+	double gety() { return y; }			   //получение значения у
+	void sum(Vector2d); //сложение векторов
+	void sub(Vector2d); //вычитание векторов
+	void mult(double); //умножение вектора на число
+	double scalarMult(Vector2d); //скалярное умножение векторов
+	void print(void); //вывод координат вектора
+	double angle(Vector2d); //угол между 2 векторами
+	double length(); //длина вектора
+	Vector2d operator+(const Vector2d& vector); //перегрузка операции сложения
+	Vector2d operator-(const Vector2d& vector); //перегрузка операции вычитания
+	double operator*(const Vector2d& vector);	//перегрузка операции умножения вектора на вектор (скалярное произведение)
+	Vector2d operator*(const double number);    //перегрузка операции умножения вектора на число
+	friend Vector2d operator*(double, Vector2d&);  //перегрузка операции умножения вектора на число
+	Vector2d& operator++();	  //перегрузка операции инкремента				   
+	Vector2d operator++(int); //перегрузка операции инкремента		   
+	Vector2d& operator--();	  //перегрузка операции декремента				  
+	Vector2d operator--(int); //перегрузка операции декремента					   
+	const Vector2d& operator += (const Vector2d&); //перегрузка операции присвоения после сложения
+	const Vector2d& operator -= (const Vector2d&); //перегрузка операции присвоения после вычитания
+	const Vector2d& operator *= (const double);    //перегрузка операции присвоения после умножения
+	operator string(); //перегрузка операции явного преобразования в string
+};
---
+++ Vector2dExample.cpp
@@ -0,0 +1,28 @@
+﻿// Vector2dExample.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "pch.h"
+#include <iostream>
+#include <clocale>
+#include "Vector2d.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Vector2d v1(2, 2);
+	Vector2d v2(1, 1, 3, 4);
+	cout << "Первый вектор: " << (string)v1 << endl;
+	cout << "Воторой вектор " << (string)v2 << endl;
+	Vector2d v3(v1);
+	cout << "Копируем значение первого вектора во третий:" << (string)v3 << endl;
+	v3.setx(5); v3.sety(4);
+	cout << "Меняем координаты третьего вектора: " << (string)v3 << endl;
+	cout << "Сумма первого и второго вектора: " << (string)(v1 + v2) << endl;
+	v3 -= v2;
+	cout << "Вычитание из третьего вектора второго: " << (string)(v3) << endl;
+	v3 *= 3;
+	cout << "Умножение третьего вектора на 3: " << (string)(v3) << endl;
+	cout << "Длина второго вектора = " << v2.length() << endl;
+	cout << "Скалярное умножение первого и второго вектора = " << v1 * v2 << endl;
+	cout << "Косинус угла между первым и вторым вектором = " << v1.angle(v2) << endl;
+}
